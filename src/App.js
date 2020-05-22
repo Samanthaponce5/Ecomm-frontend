abffdc9c105dddec231a10ebbd3d65f36fc55070
JohnsonNYC <jkow95@gmail.com>
2020-05-22 10:21:20 -0400

    Messing around with Modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import Modal from 'react-modal'
 import './App.css';
 
 function App() {
+  const [modalIsOpen,setModalIsOpen] =useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>setModalIsOpen(true)}> Open Modal</button>
+      <Modal 
+      isOpen={modalIsOpen} 
+      onRequestClose={()=> setModalIsOpen(false)} 
+      style={
+        {
+          overlay:{
+            background: 'grey'
+          },
+          content:{
+            color:'orange'
+          }
+        }
+      }
+      >
+        <h2> Modal title</h2>
+        <p> Modal body</p>
+        <div>
+          <button onClick={()=>setModalIsOpen(false)}> Close </button>
+        </div>
+      </Modal>
     </div>
   );
 }

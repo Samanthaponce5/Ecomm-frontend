721e1cc4cfc4c043ac2c1280b2c3f7a6b599b4f7
JohnsonNYC <jkow95@gmail.com>
2020-05-25 10:11:20 -0400

    Made the products component a child of home but it overlays each other

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Profile from './component/Profile'
 import Home from './component/Home'
 import CreateAccount from './component/CreateAccount'
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Products from './component/Products';
+// import Products from './component/Products';
 
 let productsURL = 'http://localhost:4000/products'
 
@@ -62,33 +62,30 @@ class App extends React.Component {
     })
   }
 
-  addToCart = (product)=>{
-    this.setState({ cart: [...this.state.cart,product] });
+  addToCart = (product) => {
+    this.setState({ cart: [...this.state.cart, product] });
   }
 
   render() {
     console.log(this.state)
     return (
       <Fragment>
-
+        <div>
         <NavBar currentUser={this.state.CurrentUser} logout={this.logout} />
+        </div>
         <Switch>
-          <Route exact path='/' component={Home} />
+          {/* <Route exact path='/' component={Home} /> */}
+          <Route exact path='/' render={(props) => <Home routerProps={props} products={this.state.products} addToCart={this.addToCart} />} />
           <Route exact path='/login' render={(props) => <Login setCurrentUser={this.setCurrentUser} routerProps={props} />} />
           <Route exact path='/create_account' render={(props) => <CreateAccount updateCurrentUser={this.updateCurrentUser} routerProps={props} />} />
           <Route exact path='/sellProduct' render={(props) => <NewProductForm currentUser={this.state.CurrentUser} updateCurrentProduct={this.updateCurrentProduct} routerProps={props} />} />
-          <Route exact path='/product' render={(props) => <Products currentUser={this.state.CurrentUser} currentProduct={this.state.CurrentProduct} currentImage={this.state.CurrentImage} routerProps={props} products={this.state.products} addToCart={this.addToCart}/>} />
-
-
+          {/* <Route exact path='/product' render={(props) => <Products currentUser={this.state.CurrentUser} currentProduct={this.state.CurrentProduct} currentImage={this.state.CurrentImage} routerProps={props} products={this.state.products} addToCart={this.addToCart}/>} /> */}
           <Route exact path='/profile' render={(props) => {
             return this.state.CurrentUser ? (
-
               <Profile currentUser={this.state.CurrentUser} />
-
             ) : (
                 <Login setCurrentUser={this.setCurrentUser} />
                 // <Redirect to='/login' />
-
               )
           }
           } />

a4bef07ef20d555e7361355d63b41d242adeeda1
JohnsonNYC <jkow95@gmail.com>
2020-05-26 18:10:43 -0400

    working on cart button user validation

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
-import {withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 class ProductCard extends Component {
-    handleClick=()=>{
+    handleClick = () => {
         this.props.routerprops.history.push(`/products/${this.props.product.id}`)
     }
     render() {
-      
+
         return (
             <>
-            
-            <div  className="grid-item-products">
-                 <div className="wrapper">      
-                 <div className="image-wrapper">
-                <img onClick={this.handleClick}  className="product-pic" src={this.props.product.image} alt="img" />
+
+                <div className="grid-item-products">
+                    <div className="wrapper">
+                        <div className="image-wrapper">
+                            <img onClick={this.handleClick} className="product-pic" src={this.props.product.image} alt="img" />
+                        </div>
+                    </div>
+
+
+                    {/* <p>${this.props.product.price} </p> */}
+                    <button className="cart-btn" onClick={() => this.props.addToCart(this.props.product)}> Add to Cart </button>
                 </div>
-         </div>
-             
-             
-                {/* <p>${this.props.product.price} </p> */}
-                <button className="cart-btn" onClick={()=>this.props.addToCart(this.props.product)}> Add to Cart </button>
-                </div>
-                </>
-           
+            </>
+
         );
     }
 }

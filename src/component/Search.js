2525717e1d95bd93b6bc32ed46f63ee7b3218e93
JohnsonNYC <jkow95@gmail.com>
2020-05-26 10:58:31 -0400

    filter logic for buttons are now active

--- src/component/Search.js
+++ src/component/Search.js
@@ -9,7 +9,7 @@ export default class Search extends React.Component{
 render(){
     return(
         <div className="search__container">
-  
+
         <input className="search__input" type="text" placeholder="Search"/>
     </div>
     )

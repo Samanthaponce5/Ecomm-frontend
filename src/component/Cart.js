6ffb773780afabe0ffe91511b2347fa3e6290391
JohnsonNYC <jkow95@gmail.com>
2020-05-28 10:31:27 -0400

    re-did confirmation page

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -7,36 +7,38 @@ class Cart extends Component {
     render() {
 
         console.log(this.props)
-        const {cart} = this.props
+        const { cart } = this.props
         let productQTY = {}
 
         // method to see how many of each product in cart
         const recordQTY = product => {
-          Object.keys(productQTY).includes(product.title) ? productQTY[product.title] += 1 : productQTY[product.title] = 1
+            Object.keys(productQTY).includes(product.title) ? productQTY[product.title] += 1 : productQTY[product.title] = 1
         }
-    
+
         // method to return an array of unique products by title to present in html
         const removeDuplicates = (myArr, prop) => {
-          return myArr.filter((obj, pos, arr) => {
-              return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos
-          })
+            return myArr.filter((obj, pos, arr) => {
+                return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos
+            })
         }
-    
+
         // value from method that removes duplicate products from cart 
         let removedProductsDuplicates = removeDuplicates(cart, 'title')
-    
+
         // method call to populate productQTY obj that sees how many products are in the cart
         cart.forEach(recordQTY)
-    //   console.log("showww",removedProductsDuplicates)
+        //   console.log("showww",removedProductsDuplicates)
 
         return (
             <div className='cart'>
-                {removedProductsDuplicates.map((cart,index) => (
-                    <CartCard key={index} product={cart} quantity={productQTY[cart.title] } addToCart={this.addToCart}/>
+                {removedProductsDuplicates.map((cart, index) => (
+                    <CartCard key={index} product={cart} quantity={productQTY[cart.title]} addToCart={this.addToCart} />
                 ))}
-               <p className='checkoutotal'>Total: ${this.props.total}</p>
-                <button className='checkoutbtn' onClick={()=> this.props.toggleCheckout(this.props.cart)}> Checkout </button>            
-                </div>
+                <p className='checkoutotal'>Total: ${this.props.total}</p>
+                <button className='checkoutbtn' onClick={() => this.props.toggleCheckout(this.props.cart)}>
+                    <Link to='/confirmation'>Checkout</Link>
+                </button>
+            </div>
 
         )
     }
